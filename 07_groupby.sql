685644aab213d099545ccbb40df23af4dbd2c595
222hojun <sciboviator@gmail.com>
2024-03-05 13:57:17 +0900

    grouping having 절 추가

--- 07_groupby.sql
+++ 07_groupby.sql
@@ -71,5 +71,26 @@ group by
     menu_price
 with rollup;
 
+select 
+	menu_price,
+    category_code
+from
+	tbl_menu
+group by
+	category_code
+having category_code between 5 and 8; -- where 가 사용이 안된다
 
+select
+	menu_price,
+    category_code,
+    menu_name
+from 
+	tbl_menu
+group by
+	category_code
+having
+	menu_name like '민트';
+
+-- group by 를 쓴 쿼리의 경우 where 이 아닌 having 으로 조건을 집어넣는다 ( 또한 select 로 조회한 것만 having을 쓸수 있다)
+    
     
